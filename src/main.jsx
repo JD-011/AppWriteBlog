a7c3ca3bb0f7edd6904c05121be4d48c61343c33
JD-011 <jayveerdahiya011@gmail.com>
2025-07-15 23:28:25 +0530

    Fixed the issue with routing on vercel app

--- src/main.jsx
+++ src/main.jsx
@@ -68,7 +68,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Provider store={store}>
-            <RouterProvider router={router} />
+            <RouterProvider router={router} basename="/" />
         </Provider>
     </StrictMode>
 )
